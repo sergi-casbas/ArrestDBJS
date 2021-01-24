e8f3c039592e61fe45db4eebe0477aa3262e8044
Sergi Casbas <sergi@casbas.cat>
2021-01-24 22:10:13 +0100

    Added a template in multiplexing path to help to install it.

---
+++ multiplexing/template.php
@@ -0,0 +1,7 @@
+<?php
+# This file name must be equal to the database sent header.
+$dsn = 'sqlite:///./mydatabase.sqlite3';
+$clients = ['127.0.0.1'];
+$api_key = 'myapikey_secret';
+$CORS_Origin = '*';
+?>
