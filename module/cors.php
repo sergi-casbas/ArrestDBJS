4bbfc758ccef2c83369850962e8d79307c927822
Sergi Casbas <sergi@casbas.cat>
2021-02-11 22:15:51 +0100

    Added disclaimer to cors module about CORS security implications.

--- module/cors.php
+++ module/cors.php
@@ -1,4 +1,8 @@
 <?php
+# WARNING: Enabling CORS module can allow any malicious web page 
+# read information from your database. Use it with caution.
+# More info on: https://en.wikipedia.org/wiki/Cross-origin_resource_sharing
+
 # Set CORS headers if $CORS_Origin variable is set.
 if ( $CORS_Origin != '' )
 {
