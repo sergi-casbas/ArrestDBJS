cae24f637a0842937040bb985f4fc627f11a2a3f
Sergi Casbas <sergi@casbas.cat>
2021-02-11 22:00:23 +0100

    Implement autentication based on statefull token (TOTP technology).

--- module/authentication.php
+++ module/authentication.php
@@ -9,9 +9,6 @@ function generate_auth_key($key, $ttl, $iterations)
     return $buffer;
 }
 
-# If api_key variable is not set, don't use autentication or authorization 
-# for backward compatibility.
-if ( $api_key != '' )
 # Function to validate if a bearer is correct or not (if not exists is wrong).
 function authorization_bearer_is_valid($headers, $bearer)
 {
@@ -21,36 +18,6 @@ function authorization_bearer_is_valid($headers, $bearer)
     # Only bearer type is allowed.
     if (strtolower($authorization[0]) != 'bearer') {return false;}
 
-    # General prerequisites and constants.
-    include_once "library/authentication.php";
-    define("AUTH_TTL",3600);
-    define("AUTH_ITERATIONS",2);
-
-    # Generate the current bearer only once to save computation cycles.
-    # Add database name if exists to avoid API_KEY colission on diffente databases.
-    $current_bearer = generate_auth_key(apache_request_headers()['Database'].$api_key, AUTH_TTL, AUTH_ITERATIONS);
-
-    # If authorization is not supplied, try autentication.
-    if (apache_request_headers()['Authorization'] == '')
-    {
-        if ( apache_request_headers()['Apikey'] == $api_key )
-        { 
-            header('Authorization: Bearer '.$current_bearer);
-            exit(ArrestDB::Reply(ArrestDB::$HTTP[200]));
-        }else{
-            header('Authorization: autentication fail');
-            exit(ArrestDB::Reply(ArrestDB::$HTTP[403]));
-        }
-    }else{ # If authorization is supplied check if is valid or not.
-        if ( ! authorization_bearer_is_valid(apache_request_headers(), $current_bearer) )
-        {
-            header('Authorization: invalid');
-            exit(ArrestDB::Reply(ArrestDB::$HTTP[403]));
-        }
-    }
-
-    # Always update current bearer on correct answers.
-    header('Authorization: Bearer '.$current_bearer);
     # Return if authorization bearer supplied is equal to the computed bearer.
     return $authorization[1] == $bearer;
 }
